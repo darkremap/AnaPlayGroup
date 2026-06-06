9249d6bf9bca6838d13878b9b3e993d18c96cdf3
Mohammad <remap0095@yahoo.com>
2026-06-06 22:38:24 +0330

    Create gallery and title

--- front-page.php
+++ front-page.php
@@ -1,1 +1,49 @@
-<?php get_header(); ?><h1>Home Page</h1><?php get_footer(); ?>
+<?php get_header(); ?>
+
+
+
+<section class="AnaGames">
+    <div class="AnaGames-Title">
+        <h1>هر بازی یک سفر</h1>
+        <div class="AnaTitleBoarder">
+            <span class="divider-dot"></span>
+            <span class="divider-line"></span>
+            <span class="divider-diamond"></span>
+            <span class="divider-line"></span>
+            <span class="divider-dot"></span>
+        </div>
+        <div class="AnaGames-Content">
+            <p class="iranSans">بازی‌های ما صرفاً برای سرگرمی نیستند.</p>
+            <p class="iranSans">آن‌ها فضایی برای کنجکاوی، تأمل و آگاهی عمیق‌تر نسبت به خود و دیگران ایجاد می‌کنند.</p>
+        </div>
+   </div>
+
+    <div class="AnaGames-gallery">
+        <div class="AnaGames-panel">
+            <img src="https://picsum.photos/id/1015/800/800" alt="">
+            <span>#23</span>
+        </div>
+
+        <div class="AnaGames-panel">
+            <img src="https://picsum.photos/id/1016/800/800" alt="">
+            <span>#24</span>
+        </div>
+
+        <div class="AnaGames-panel">
+            <img src="https://picsum.photos/id/1018/800/800" alt="">
+            <span>#25</span>
+        </div>
+
+        <div class="AnaGames-panel">
+            <img src="https://picsum.photos/id/1020/800/800" alt="">
+            <span>#26</span>
+        </div>
+
+        <div class="AnaGames-panel">
+            <img src="https://picsum.photos/id/1024/800/800" alt="">
+            <span>#27</span>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
