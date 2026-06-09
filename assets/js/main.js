f0ecd3832c6764ca8c4424629519b6e9a745e16a
Mohammad <remap0095@yahoo.com>
2026-06-09 20:58:35 +0330

    add the mobile style and the text

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,12 +30,14 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-// AnaGames Section Fore Drower Hover 
+// AnaGames Section Fore Drower Hover
 const panels = document.querySelectorAll('.AnaGames-panel');
 panels.forEach(panel => {
-  panel.addEventListener('mouseenter', () => {
-    panels.forEach(p => p.classList.remove('active'));
-    panel.classList.add('active');
+  ['mouseenter', 'click'].forEach(evt => {
+    panel.addEventListener(evt, () => {
+      panels.forEach(p => p.classList.remove('active'));
+      panel.classList.add('active');
+    });
   });
 });
 
